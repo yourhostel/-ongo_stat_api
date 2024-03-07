eaa719d7f9ec0638f0d80945fe95cb984a3ba089
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-03-07 03:55:51 +0200

    corrected

--- src/main/java/com/example/stat/config/MongoConfig.java
+++ src/main/java/com/example/stat/config/MongoConfig.java
@@ -2,6 +2,7 @@ package com.example.stat.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -9,14 +10,25 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 public class MongoConfig {
 
+    @Value("${myapp.mongodb.username}")
+    private String username;
+
+    @Value("${myapp.mongodb.password}")
+    private String password;
+
+    @Value("${myapp.mongodb.database}")
+    private String database;
+
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://root:example@localhost:27017");
+        return MongoClients
+                .create(String.format("mongodb://%s:%s@localhost:27017/",
+                        username, password));
     }
 
     @Bean
     public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "stat");
+        return new MongoTemplate(mongoClient(), database);
     }
 
 }
